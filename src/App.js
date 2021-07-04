56e46f24b37440fc3fd4fee14fc4ba8b7e47cb4e
Nicolas <nicksiuxs@gmail.com>
2021-07-04 18:43:21 -0500

    ADD: get first charge gifs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const apiKey = 'UkMGUnKzhz34irLeeI5dSO2YGC5j1wts';
+
 function App() {
-	return <div className="App">Aplicación</div>;
+	const [gifs, setGifs] = useState([]);
+	const keyword = 'pandas';
+	const urlAPI = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en"`;
+	useEffect(() => {
+		fetch(urlAPI)
+			.then((res) => res.json())
+			.then((response) => {
+				const data = response.data;
+				const newGifs = data.map((file) => {
+					// console.log(file.images.fixed_height.url);
+					return file.images.fixed_height.url;
+				});
+				setGifs(newGifs);
+			});
+	}, []);
+	return (
+		<div className="App">
+			<h1>Giffy</h1>
+			{gifs.map((gif, index) => {
+				return <img key={index} src={gif} alt="gifs" />;
+			})}
+		</div>
+	);
 }
 
 export default App;
