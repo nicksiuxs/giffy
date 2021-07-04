02b6c6b9b42e3d508b959a993d102bc0d71ffd9e
Nicolas <nicksiuxs@gmail.com>
2021-07-04 17:48:43 -0500

    ADD: plugin jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
-const RULES ={
-	OFF: "off",
-	WARN: "warn",
-	ERROR: "error"
-}
+const RULES = {
+	OFF: 'off',
+	WARN: 'warn',
+	ERROR: 'error',
+};
 
 module.exports = {
 	env: {
@@ -10,21 +10,25 @@ module.exports = {
 		node: true,
 		es2021: true,
 	},
-	extends: ["eslint:recommended", "plugin:react/recommended"],
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:jest/recommended',
+	],
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
 		},
 		ecmaVersion: 12,
-		sourceType: "module",
+		sourceType: 'module',
 	},
-	plugins: ["react"],
+	plugins: ['react', 'jest'],
 	rules: {
-		indent: ["error", "tab"],
-		"linebreak-style": ["error", "windows"],
-		quotes: ["error", "single"],
-		semi: ["error", "always"],
-		"react/react-in-jsx-scope": RULES.OFF,
-		"react/prop-types" : RULES.OFF,
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'windows'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'react/react-in-jsx-scope': RULES.OFF,
+		'react/prop-types': RULES.OFF,
 	},
 };
