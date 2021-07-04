aa3eb6a1ea610d41d840e50b832ea2c7721a30ac
Nicolas <nicksiuxs@gmail.com>
2021-07-04 17:21:51 -0500

    ADD: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,30 @@
+const RULES ={
+	OFF: "off",
+	WARN: "warn",
+	ERROR: "error"
+}
+
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
+	env: {
+		browser: true,
+		node: true,
+		es2021: true,
 	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended'
-	],
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
+	extends: ["eslint:recommended", "plugin:react/recommended"],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
 		},
-		'ecmaVersion': 12,
-		'sourceType': 'module'
+		ecmaVersion: 12,
+		sourceType: "module",
 	},
-	'plugins': [
-		'react'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+	plugins: ["react"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "single"],
+		semi: ["error", "always"],
+		"react/react-in-jsx-scope": RULES.OFF,
+		"react/prop-types" : RULES.OFF,
+	},
 };
