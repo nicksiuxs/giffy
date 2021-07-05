319b29f2e9d65dd4ad5ec6261808b0bd37f0e470
Nicolas <nicksiuxs@gmail.com>
2021-07-04 23:11:11 -0500

    ADD: detail component

---
+++ src/pages/Detail/Detail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './Detail.css';
+
+const Detail = ({ params }) => {
+	console.log(params);
+	return <div className="detail">detalle</div>;
+};
+
+export default Detail;
