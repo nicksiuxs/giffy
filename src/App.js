c21027be4220155fd719fa12771256adb2603713
Nicolas <nicksiuxs@gmail.com>
2021-07-04 20:08:06 -0500

    ADD: wouter in app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import { useEffect, useState } from 'react';
+import { Route } from 'wouter';
 import './App.css';
-
-const apiKey = 'UkMGUnKzhz34irLeeI5dSO2YGC5j1wts';
+import ListOfGifs from 'src/components/ListOfGifs/ListOfGifs';
 
 function App() {
-	const [gifs, setGifs] = useState([]);
-	const keyword = 'pandas';
-	const urlAPI = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en"`;
-	useEffect(() => {
-		fetch(urlAPI)
-			.then((res) => res.json())
-			.then((response) => {
-				const data = response.data;
-				const newGifs = data.map((file) => {
-					// console.log(file.images.fixed_height.url);
-					return file.images.fixed_height.url;
-				});
-				setGifs(newGifs);
-			});
-	}, []);
 	return (
 		<div className="App">
 			<h1>Giffy</h1>
-			{gifs.map((gif, index) => {
-				return <img key={index} src={gif} alt="gifs" />;
-			})}
+			<Route path="/search/:keyword" component={ListOfGifs} />
 		</div>
 	);
 }
