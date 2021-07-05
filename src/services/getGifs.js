3857a48b83973ebcd5b90c7aea3a5a985a3c34d1
Nicolas <nicksiuxs@gmail.com>
2021-07-04 19:17:13 -0500

    ADD: function get gifs

---
+++ src/services/getGifs.js
@@ -0,0 +1,21 @@
+const { REACT_APP_API_KEY } = process.env;
+
+export default function getGifs({ keyword = 'Morty' } = {}) {
+	const urlAPI = `https://api.giphy.com/v1/gifs/search?api_key=${REACT_APP_API_KEY}&q=${keyword}&limit=25&offset=0&rating=g&lang=en"`;
+	return fetch(urlAPI)
+		.then((res) => res.json())
+		.then((response) => {
+			const { data = [] } = response;
+			if (Array.isArray(data)) {
+				const newGifs = data.map((file) => {
+					const { images, title, id } = file;
+					const { url } = images.fixed_height;
+					return { title, id, url };
+				});
+
+				return newGifs;
+			} else {
+				return [];
+			}
+		});
+}
