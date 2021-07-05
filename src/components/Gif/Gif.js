95b6589ccfc1ad6c0deda22a8eadec0a5d2f8f87
Nicolas <nicksiuxs@gmail.com>
2021-07-04 23:11:45 -0500

    REFACTOR: change Link route in gif

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -3,7 +3,7 @@ import './Gif.css';
 
 const Gift = ({ title, id, url }) => {
 	return (
-		<a href={`#${id}`} className="gif">
+		<a href={`/gif/${id}`} className="gif">
 			<img src={url} alt={title} />
 			<h4>{title}</h4>
 		</a>
