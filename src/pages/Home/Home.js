f03f3335c7218e3b5402ea53e2b7e08b8a1c9d58
Nicolas <nicksiuxs@gmail.com>
2021-07-04 23:32:58 -0500

    ADD: search bar in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,44 @@
-import { Link } from 'wouter';
+import { useState } from 'react';
+import { Link, useLocation } from 'wouter';
 
 import './Home.css';
 
+const POPULAR_GIFS = ['Pandas', 'Colombia', 'Argentina'];
+
 const Home = () => {
+	const [keyword, setKeyword] = useState('');
+
+	const [path, pushLocation] = useLocation();
+
+	const handleSubmit = (e) => {
+		// Navegar a otro lado
+		e.preventDefault();
+		pushLocation('/search/' + keyword);
+	};
+
+	const handleChange = (e) => {
+		setKeyword(e.target.value);
+	};
+
 	return (
 		<div className="home">
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Busca tu gif"
+					onChange={handleChange}
+					value={keyword}
+				/>
+			</form>
 			<h2>Los gifs más populares</h2>
 			<ul>
-				<li>
-					<Link to="/search/pandas">Pandas</Link>
-				</li>
-				<li>
-					<Link to="/search/ny">New York</Link>
-				</li>
-				<li>
-					<Link to="/search/fornite">Fornite</Link>
-				</li>
+				{POPULAR_GIFS.map((route, key) => {
+					return (
+						<li key={key}>
+							<Link to={'/search/' + route}>GIF de {route}</Link>
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
