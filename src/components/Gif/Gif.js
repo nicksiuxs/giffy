dcd0856890d6e425f412df3f2774c75f44749c96
Nicolas <nicksiuxs@gmail.com>
2021-07-05 22:13:29 -0500

    ADD: loading lazy in gif

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -4,7 +4,7 @@ import './Gif.css';
 const Gift = ({ title, id, url }) => {
 	return (
 		<a href={`/gif/${id}`} className="gif">
-			<img src={url} alt={title} />
+			<img loading="lazy" src={url} alt={title} />
 			<h4>{title}</h4>
 		</a>
 	);
