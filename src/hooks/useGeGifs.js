0dab3ce2e8085c5b2188d4d2f32e7d2cf82a7692
Nicolas <nicksiuxs@gmail.com>
2021-07-05 22:13:00 -0500

    ADD: last keyword search

--- src/hooks/useGeGifs.js
+++ src/hooks/useGeGifs.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from 'react';
 import getGifs from 'src/services/getGifs.js';
 
-export default function useGetGifs({ keyword }) {
+export default function useGetGifs({ keyword } = { keyword: null }) {
 	const [gifs, setGifs] = useState([]);
 
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		setLoading(true);
-		getGifs({ keyword }).then((res) => {
+		// recuperamos la última palabra del localstorage
+		const keywordToUse =
+			keyword || localStorage.getItem('lastKeyword') || 'random';
+		getGifs({ keyword: keywordToUse }).then((res) => {
 			setGifs(res);
 			setLoading(false);
+			// Guardamos la última palabra buscada
+			localStorage.setItem('lastKeyword', keyword);
 		});
 	}, [keyword]);
 
