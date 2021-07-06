6cea4892547db8ec3022250735e5f4a3fb9a3117
Nicolas <nicksiuxs@gmail.com>
2021-07-05 21:33:43 -0500

    REFACTOR: add list of gifs component

--- src/pages/SearchList/SearchList.js
+++ src/pages/SearchList/SearchList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Gif from 'src/components/Gif/Gif';
 import Loader from 'src/components/Loader/Loader';
+import ListOfGifs from 'src/components/ListOfGifs/ListOfGifs';
 
 import useGetGifs from 'src/hooks/useGeGifs';
 
@@ -12,19 +12,7 @@ const SearchList = ({ params }) => {
 
 	const { loading, gifs } = useGetGifs({ keyword });
 
-	return (
-		<>
-			{loading ? (
-				<Loader />
-			) : (
-				<div className="list-gifs">
-					{gifs.map(({ id, title, url }) => (
-						<Gif key={id} title={title} id={id} url={url} />
-					))}
-				</div>
-			)}
-		</>
-	);
+	return <>{loading ? <Loader /> : <ListOfGifs gifs={gifs} />}</>;
 };
 
 export default SearchList;
