f5a6537b22723695f6c5c6e19ba565c537c12002
Nicolas <nicksiuxs@gmail.com>
2021-07-09 21:57:47 -0500

    REFACTOR: change a for Link in gif

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'wouter';
 import './Gif.css';
 
 const Gift = ({ title, id, url }) => {
 	return (
-		<a href={`/gif/${id}`} className="gif">
+		<Link to={`/gif/${id}`} className="gif">
 			<img loading="lazy" src={url} alt={title} />
 			<h4>{title}</h4>
-		</a>
+		</Link>
 	);
 };
 
